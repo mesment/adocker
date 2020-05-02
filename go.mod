0f5b786741cb03680eb4a2851d19a884c0dc7d5a
wanglei <wanglei@33.cn>
2020-05-02 10:40:03 +0800

    add reexec

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mesment/adocker
 
 go 1.14
+
+require (
+	github.com/docker/docker-ce v17.12.1-ce-rc2+incompatible
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+)
--- main.go
+++ main.go
@@ -7,7 +7,34 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"github.com/docker/docker-ce/components/engine/pkg/reexec"
 )
+
+func init()  {
+	reexec.Register("nsInitialisation", nsInitialisation)
+	if reexec.Init() {
+		os.Exit(0)
+	}
+}
+
+func nsInitialisation() {
+	log.Printf("\n>> namespace setup code goes here <<\n\n")
+	nsRun()
+}
+func nsRun() {
+	cmd := exec.Command("/bin/sh")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Env = []string{"PS1=-[adocker-process]- # "}
+
+	if err := cmd.Run(); err != nil {
+		log.Printf("Error running the /bin/sh command - %s\n", err)
+		os.Exit(1)
+	}
+}
 
 func main()  {
 	cmd := exec.Command("sh")
