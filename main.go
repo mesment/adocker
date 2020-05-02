e698cce3705465e0925306e1f7a1276c06da108b
wanglei <wanglei@33.cn>
2020-05-02 09:59:31 +0800

    add user mapping

--- main.go
+++ main.go
@@ -12,12 +12,32 @@ import (
 func main()  {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:syscall.CLONE_NEWUTS|syscall.CLONE_NEWIPC|syscall.CLONE_NEWPID|syscall.CLONE_NEWNS|syscall.CLONE_NEWUSER|syscall.CLONE_NEWNET,
+		Cloneflags:syscall.CLONE_NEWUTS|
+			syscall.CLONE_NEWIPC|
+			syscall.CLONE_NEWPID|
+			syscall.CLONE_NEWNS|
+			syscall.CLONE_NEWNET|
+			syscall.CLONE_NEWUSER,
+			UidMappings:[]syscall.SysProcIDMap{
+				{
+					ContainerID: 0,
+					HostID:      os.Getuid(),
+					Size:        1,
+				},
+			},
+			GidMappings: []syscall.SysProcIDMap{
+					{
+						ContainerID: 0,
+						HostID:      os.Getgid(),
+						Size:        1,
+					},
+			},
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		log.Printf("Error running the /bin/sh command - %s\n", err)
+		panic(err)
 	}
 }
